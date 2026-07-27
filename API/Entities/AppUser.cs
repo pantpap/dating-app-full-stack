9a332dd99cfba9ef803f04c54d07e91beeea15e5
Pantelis Papasavvas <pantpap@gmail.com>
2026-07-28 00:27:20 +0300

    feat(database): added dbContext configuration

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,6 +1,6 @@
 namespace API.Entities;
 
-public class AppUser
+public class AppUser 
 {
     public string Id { get; set; } = Guid.NewGuid().ToString();
     public required string DisplayName { get; set; } 
--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,17 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+   opt.UseSqlite(builder.Configuration.GetConnectionString("Default Connection"));
+});
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
