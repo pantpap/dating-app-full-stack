24612bdb84b2e538b61075a3ec28e89b64893056
Pantelis Papasavvas <pantpap@gmail.com>
2026-09-03 13:24:44 +0300

    feat(members): create controller and endopoint to fetch all member and members by id

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fc36fa1a581098390d4a4a3b2a98377bc31d6ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eca0b1594fa3c7fdbf885c6e0ebabeeb125488a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
